0ca3d468d797815a54feaac3346247e75fc6255b
np303 <2ep303@gmail.com>
2019-05-28 11:53:16 -0500

    Creasted Exercises and Hello.java!

---
+++ src/org/launchcode/java/exercises/Hello.java
@@ -0,0 +1,24 @@
+package org.launchcode.java.exercises;
+
+import java.util.Scanner;
+
+/**
+ * Written by Nick Paisley
+ * An app thats asks for your name and returns a greeting + yourname
+ */
+
+public class Hello {
+    public static void main(String[] args) {
+        String yourname;
+        Scanner in;
+
+        in = new Scanner(System.in);
+        System.out.println("What is your name?");
+
+        yourname = in.next();
+        System.out.println("Hello," + yourname);
+
+
+    }
+
+}
