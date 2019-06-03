310236098411bb140ed437cb3014db609155c4af
np303 <2ep303@gmail.com>
2019-06-03 16:26:10 -0500

    modified  Arraylist exercise to initialize array in one line

--- src/org/launchcode/java/exercises/SumofArrayList.java
+++ src/org/launchcode/java/exercises/SumofArrayList.java
@@ -9,17 +9,8 @@ public class SumofArrayList {
 
     public static void main(String[] args) {
 
-        ArrayList<Integer> numbers = new ArrayList<Integer>();
-        numbers.add(1);
-        numbers.add(2);
-        numbers.add(3);
-        numbers.add(4);
-        numbers.add(5);
-        numbers.add(6);
-        numbers.add(7);
-        numbers.add(8);
-        numbers.add(9);
-        numbers.add(10);
+        ArrayList<Integer> numbers = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+
 
         int sum = 0;
 
